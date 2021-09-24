064e40bc4107e52dc793f2089ba6a9b69373ff0b
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-09-23 21:06:16 -0400

    env

--- src/components/_helpers/environment.js
+++ src/components/_helpers/environment.js
@@ -4,7 +4,7 @@ switch(window.location.hostname) {
   case 'localhost' || '127.0.0.1':
     API_URL = 'http://localhost:3333'
     break;
-  case 'https://tmor-pet-tinder-client.herokuapp.com':
+  case 'tmor-pet-tinder-client.herokuapp.com':
     API_URL = 'https://tmor-pet-tinder-client.herokuapp.com'
 }
 
