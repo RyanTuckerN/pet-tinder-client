d5b2408df6bfee40ae96f55dd54690b9aff62475
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-09-23 22:37:11 -0400

    localhost

--- src/components/_helpers/environment.js
+++ src/components/_helpers/environment.js
@@ -2,7 +2,7 @@ let API_URL = ''
 
 switch(window.location.hostname) {
   case 'localhost' || '127.0.0.1':
-    API_URL = 'http://localhost:3000'
+    API_URL = 'http://localhost:3333'
     break;
   case 'tmor-pet-tinder-client.herokuapp.com':
     API_URL = 'https://tmor-pet-tinder.herokuapp.com'
