a697a52ae0e695cd4a9368c94841a438da66f21d
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-09-28 20:43:13 -0400

    chnged alert to !

--- src/components/MainLayoutComponents/MatchList.jsx
+++ src/components/MainLayoutComponents/MatchList.jsx
@@ -75,9 +75,9 @@ const MatchList = (props) => {
                 <ListItemAvatar>
                   <div className="avatar-wrapper">
                     <Badge 
-                    badgeContent={notificationsHash[match.id]}
+                    badgeContent='!'
                     // variant='dot'
-                    // invisible={notificationsHash[match.id]?false:true}
+                    invisible={notificationsHash[match.id]?false:true}
                      color='secondary'  >
                       <Avatar
                         src={match.photo_url}
